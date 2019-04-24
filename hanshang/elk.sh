f2dc9317ce6059deed5f07059747174d280dbe41
a6480078 <513957882@qq.com>
2019-04-24 08:48:40 +0800

    first commit

--- hanshang/elk.sh
+++ hanshang/elk.sh
@@ -83,3 +83,66 @@ nohup bin/kibana &
 ###########配置和启动filebeat##########################################
 cd /usr/local/filebeat-6.2.3-linux-x86_64
 
+###########配置和启动elastalert#########################################
+
+git clone https://github.com/Yelp/elastalert.git
+pip install --upgrade
+pip install "setuptools>=11.3"
+python setup.py install
+pip install "elasticsearch>=5.0.0"
+########创建 ElastAlert 所需的索引#############################
+elastalert-create-index
+
+cat > config.yaml << EOF
+rules_folder: /usr/local/work/elastalert/rule
+run_every:
+  minutes: 1
+buffer_time:
+  minutes: 15
+es_host: 127.0.0.1
+es_port: 9200
+writeback_index: elastalert_status
+alert_time_limit:
+  days: 2
+EOF
+
+cat > rule/log.yaml << EOF
+name: apierror
+type: frequency
+index: logstash-*  #监控这个索引
+num_events: 3       #限定时间内,发生时间次数
+timeframe:          #下定时间 ,跟上边的合起来就是一分钟有三个错误日志写进es的话,就发送邮件
+  minutes: 1
+filter:
+- query:
+    query_string:
+      query: message:"error"
+#  - regexp:
+#      message: ".*"
+#- term:
+#    beat.name: "com-04"
+alert:
+- "email"
+smtp_host: smtp.exmail.qq.com
+smtp_port: 465
+smtp_ssl: true
+#用户认证文件，需要user和password两个属性
+smtp_auth_file: /usr/local/work/elastalert/smtpfile.yaml
+email_reply_to: "bruce.xiong@chinaentropy.com"
+from_addr: "bruce.xiong@chinaentropy.com"
+email:
+- "xiongjianan100@sina.com"
+EOF
+
+nohup python -m elastalert.elastalert --config config.yaml --verbose --rule rule/log.yaml &
+
+##################Elastalert trobleshotting#############################
+pip install --upgrade --force-reinstall pip==9.0.3
+pip install --upgrade --force-reinstall pip==9.0.3
+pip uninstall PyYAML
+pip install --upgrade pip
+pip install elastalert
+pip uninstall PyYAML
+pip install --upgrade pip
+pip install elastalert
+
